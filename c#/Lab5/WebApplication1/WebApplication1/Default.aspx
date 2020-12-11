23850ba8a6486521ffa735cb12077c8306a904fa
Vitali <vital.dz@mail.ru>
2020-12-11 17:39:14 +0300

    lab 5
    lab 5

---
+++ c#/Lab5/WebApplication1/WebApplication1/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    
+    Calculate
+    <asp:TextBox ID="textBox1" runat="server"></asp:TextBox>
+    <asp:Button ID="sumBtn" runat="server" Text="+" OnClick="sumBtn_Click"/>
+    <asp:Button ID="minusBtn" runat="server" Text="-" OnClick="minusBtn_Click"/>
+    <asp:Button ID="mulBtn" runat="server" Text="*" OnClick="mulBtn_Click"/>
+    <asp:Button ID="divBtn" runat="server" Text="/" OnClick="divBtn_Click"/>
+    <asp:TextBox ID="textBox2" runat="server"></asp:TextBox>
+    <asp:Label ID="ResultLb" runat="server" Text="Label" Visible="False"></asp:Label>
+
+
+</asp:Content>
---
+++ c#/Lab5/WebApplication1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web.UI;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+            }
+        }
+        
+        protected void sumBtn_Click(object sender, EventArgs e)
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                double sum = Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text);
+                ResultLb.Visible = true;
+                ResultLb.Text = sum.ToString();
+            }
+        }
+
+        protected void minusBtn_Click(object sender, EventArgs e)
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                double minus = Convert.ToDouble(textBox1.Text) - Convert.ToDouble(textBox2.Text);
+                ResultLb.Visible = true;
+                ResultLb.Text = minus.ToString();
+            }
+        }
+
+        protected void mulBtn_Click(object sender, EventArgs e)
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                double mult = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text);
+                ResultLb.Visible = true;
+                ResultLb.Text = mult.ToString();
+            }
+        }
+
+        protected void divBtn_Click(object sender, EventArgs e)
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                double div = Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text);
+                ResultLb.Visible = true;
+                ResultLb.Text = "         " + div.ToString();
+            }
+        }
+    }
+}
